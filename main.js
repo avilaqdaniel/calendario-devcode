50d21b6f5912b47b09ef1b74b589e0730e145bda
avilaqdaniel <avilaqdaniel@gmail.com>
2019-06-03 12:19:15 -0500

    Se termina de modificar main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 var mes_text = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
 var dia_text = ["Dom", "Lun", "Mar", "Mie", "Juv", "Vie", "Sab"];
